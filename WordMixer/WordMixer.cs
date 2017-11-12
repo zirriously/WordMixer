4cc6ad34a152dd5189a0ffa4c5db5abb85043a25
zirr <chri650t@edu.easj.dk>
2017-11-12 19:15:51 +0100

    Splitting input string into list added.

--- WordMixer/WordMixer.cs
+++ WordMixer/WordMixer.cs
@@ -1,27 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace WordMixer
 {
     public class WordMixer
     {
-        private List<string> _inputList;
+        private List<string> _inputList = new List<string>();
+        private string _output;
 
-        public void MixWords(string inputList)
+        public void MixWords(string inputString)
         {
+            Char splitter = ' ';
+            String[] subStrings = inputString.Split(splitter);
+            foreach (string subString in subStrings)
+            {
+                _inputList.Add(subString);
+            }
 
 
 
+
+            foreach (var value in _inputList)
+            {
+                _output += " " + value;
+            }
+            
 
         }
 
 
         public string Output
         {
-            get
-            {
-                return"";
-            }
+            get { return _output; }
         }
 
 
